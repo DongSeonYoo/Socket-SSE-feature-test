9a2db0d389003c650f8cded3457b174c2499a006
ehdtjs0612 <inko51366@naver.com>
2024-07-20 17:39:48 +0900

    refactor: 테스트를 위해 로그인하지 않았을 시 임시로 유저를 넣어줌

--- src/apis/auth/guards/jwt-access.guard.ts
+++ src/apis/auth/guards/jwt-access.guard.ts
@@ -9,7 +9,11 @@ import { AuthGuard } from '@nestjs/passport';
 export class JwtAccessGuard extends AuthGuard('jwt') {
   handleRequest(err: any, user: any, info: any, context: ExecutionContext) {
     if (err || !user) {
-      throw new UnauthorizedException('회원만 이용 가능한 서비스입니다');
+      // 테스트 용도로 임시로 주석 처리
+      // throw new UnauthorizedException('회원만 이용 가능한 서비스입니다');
+
+      // 테스트용: 만약 user가 없다면 임시로 2번유저로 간주
+      user = { idx: 2 };
     }
 
     return user;
