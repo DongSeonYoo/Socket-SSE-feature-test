0d366147b8508171381c783d96e140adc8628eec
ehdtjs0612 <inko51366@naver.com>
2024-07-24 13:49:43 +0900

    refactor: subject 삭제하는 콜백 등록.

--- src/apis/sse/sse.controller.ts
+++ src/apis/sse/sse.controller.ts
@@ -4,7 +4,7 @@ import { LoginAuth } from 'src/decorators/jwt-auth.decorator';
 import { LoginUser } from 'src/decorators/login-user.decorator';
 import { IAuth } from '../auth/interfaces/auth.interface';
 import { SseService } from './sse.service';
-import { Subject } from 'rxjs';
+import { Observable } from 'rxjs';
 
 @ApiTags('SSE')
 @LoginAuth()
@@ -13,7 +13,7 @@ export class SseController {
   constructor(private readonly sseService: SseService) {}
 
   @Sse('subscribe')
-  subscribe(@LoginUser() user: IAuth.IJwtPayload): Subject<MessageEvent> {
+  subscribe(@LoginUser() user: IAuth.IJwtPayload): Observable<MessageEvent> {
     return this.sseService.subscribe(user.idx);
   }
 }
--- src/apis/sse/sse.service.ts
+++ src/apis/sse/sse.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, Logger } from '@nestjs/common';
-import { Subject } from 'rxjs';
+import { finalize, Observable, Subject } from 'rxjs';
 import { IUser } from '../users/entities/user.entity';
 import { CreateNotificationDto } from '../notifications/dto/create-notification.dto';
 
@@ -8,12 +8,26 @@ export class SseService {
   private readonly logger: Logger = new Logger(SseService.name);
   private readonly clients: Map<number, Subject<MessageEvent>> = new Map();
 
-  subscribe(userIdx: IUser['idx']): Subject<MessageEvent> {
+  /**
+   * 1. 클라이언트가 연결되면 클라이언트의 idx를 key로 하여 Subject를 생성
+   * 2. 클라이언트가 연결을 끊으면 해당 클라이언트의 Subject를 삭제하는 콜백 등록
+   * 3. 이후 Observable로 변환하여 반환 (외부에서 값 넣는 행위를 방지하기 위해)
+   * @todo 메모리 누수를 방지하기 위해 클라이언트 연결을 주기적으로 정리하는 로직 추가해야할듯
+   */
+  subscribe(userIdx: IUser['idx']): Observable<MessageEvent> {
     if (!this.clients.has(userIdx)) {
-      this.clients.set(userIdx, new Subject<MessageEvent>());
+      const subject = new Subject<MessageEvent>();
+      this.clients.set(userIdx, subject);
+
+      subject.asObservable().pipe(
+        finalize(() => {
+          this.clients.delete(userIdx);
+          this.logger.debug(`Client ${userIdx} disconnected`);
+        }),
+      );
     }
 
-    return this.clients.get(userIdx);
+    return this.clients.get(userIdx).asObservable();
   }
 
   sendNotification(userIdx: IUser['idx'], data: CreateNotificationDto) {
